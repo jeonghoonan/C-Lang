73be83b3e565ffabf2a75b64b9bc3fe777fd31ad
jeonghoonan <jh3880@naver.com>
2020-09-21 21:38:34 +0900

    new lecture updated

---
+++ C/NadoCoding/NadoCoding/loop.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+
+int main(void)
+{
+	// printf("Hello World\n");
+	
+	// ++
+	/*
+	int a = 10;
+	printf("a 는 %d\n", a); // 10
+	a++;
+	printf("a 는 %d\n", a); // 11 
+	a++;
+	printf("a 는 %d\n", a); // 12
+	*/
+
+	//int b = 20;
+	//printf("b 는 %d\n", ++b); // 21 
+	//printf("b 는 %d\n", b++); // 21 
+	//printf("b 는 %d\n", b); // 22
+
+	int i = 1;
+
+	return 0;
+}
