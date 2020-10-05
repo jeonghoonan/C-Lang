62b1357c9525d52833a7211f67db64c9d7680375
jeonghoonan <jh3880@naver.com>
2020-10-05 16:36:48 +0900

    function_project uploaded

--- C/NadoMac/function.c
+++ C/NadoMac/function.c
@@ -1,20 +1,79 @@
 #include <stdio.h>
 
+// 선언
+void p(int num);
+
+void function_without_return();
+int function_with_return();
+void function_without_params();
+void function_with_params(int num1, int num2, int num3); // 3개의 정수를 받는함수이다 
+
+int apple(int total, int ate); // 전체 total 개에서 ate 를 먹고 남은 사과 수 
+
 int main_function(void) {
-	int num = 2;
-	printf("num is %d\n", num); // 2
+// 	int num = 2;
+// 	//printf("num is %d\n", num); // 2
+// 	p(num);
 
-	num = num + 3; // num += 3;
-	printf("num is %d\n", num); // 5
+// 	num = num + 3; // num += 3;
+// 	//printf("num is %d\n", num); // 5
+// 	p(num);
 
-	num -= 1; // num = num - 1;
-	printf("num is %d\n", num); // 4
+// 	num -= 1; // num = num - 1;
+// 	//printf("num is %d\n", num); // 4
+// 	p(num);
 
-	num *= 3;
-	printf("num is %d\n", num); // 12
+// 	num *= 3;
+// 	//printf("num is %d\n", num); // 12
+// 	p(num);
 
-	num /= 6;
-	printf("num is %d\n", num); // 2
+// 	num /= 6;
+// 	//printf("num is %d\n", num); // 2
+// 	p(num);
 
-	return 0;
+// 	return 0;
+// }
+
+// 함수 종류
+// 반환값이 없는 함수 
+// function_without_return();
+
+// 반환값이 있는 함수 
+// int ret = function_with_return();
+// p(ret);
+
+// 파라미터(전달값) 이 없는 함수 
+// function_without_params();
+
+// 파라미터(전달값) 이 있는 함수 
+// function_with_params(1, 2, 3);
+
+// 파라미터(전달값)도 받고, 반환값이 있는 함수 
+int ret = apple(5, 2); //there are 5 apples, but ate 2
+return 0;
 }
+
+// 정의
+void function_without_return() {
+	printf("no return value function\n");
+}
+
+int function_with_return() {
+	printf("return value function\n");
+	return 10;
+}
+
+void function_without_params() {
+	printf("there is no return params");
+}
+
+void function_with_params(int num1, int num2, int num3) {
+	printf("return value function, return value is %d, %d, %d\n", num1, num2, num3);
+}
+
+int apple(int total, int ate) {
+	printf("return ")
+	return 
+}
+
+// 반환형 함수이름(전달값)
---
+++ C/NadoMac/function_project.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h> // srand 함수를 이용하기 위해서느 <stdlib.h> 를 이용해야 한다 
+
+int getRandomNumber(int level);
+void showQuestion(int level, int num1, int num2);
+void success();
+void fail();
+
+int main(void) {
+    // 문이 5개가 있고, 각 문마다 점점 어려운 수식 퀴즈가 출제 (랜덤)
+    // 맞히면 통과, 틀리면 실패 
+
+    srand(time(NULL)); // 랜덤 / 이걸 사용하기 위해서 #include <time.h> 를 한다 
+    int count = 0; // 맞힌 횟수 
+    for (int i = 1; i < 5; i++) {
+        // x * y = ?
+        int num1 = getRandomNumber(i);
+        int num2 = getRandomNumber(i);
+        //printf("%d X %d ?", num1, num2);
+        showQuestion(i, num1, num2);
+
+        int answer = 1;
+        scanf("%d", &answer);
+        if (answer == -1) {
+            printf("close the program\n");
+            exit(0);
+        } else if (answer == num1 * num2) {
+            // successful
+            success();
+            count++;
+        } else {
+            // fail
+            fail();
+        }
+    }
+
+    printf("\n\n you answered %d amoung 5 passwords\n", count);
+    return 0;
+}
+
+int getRandomNumber(int level) {
+    return rand() % (level * 7) + 1; // 0 이 나오면 너무 쉬워서 1을 더한다 
+}
+
+void showQuestion(int level, int num1, int num2) {
+    printf("\n\n\n######## %d 번째 비밀번호 ######\n", level);
+    printf("\n\t%d x %d 는 ?\n\n", num1, num2);
+    printf("\n비밀번호를 입력하세요 (종료 : -1)>> ");
+}
+
+void success() {
+    printf("\n >> Good! answer \n");
+}
+
+void fail() {
+    printf("\n >> Bad! fail \n");
+}
